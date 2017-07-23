70735278530455a88a543882a24b1c1dc49cc05d
ikmckenz <sirreal.ian@gmail.com>
2017-07-22 22:57:46 -0600

    add 3-2

---
+++ Chapter3/3-2.c
@@ -0,0 +1,71 @@
+/* Write a function escape(s,t) that converts characters like
+newline and tab into visible escape sequences like \n and \t
+as it copies the string t to s. Use a switch. Write a function
+for the other direction as well, converting escape sequences
+into the real characters. */
+
+#include <stdio.h>
+
+void escape(char s[], char t[]);
+void antiescape(char s[], char t[]);
+
+main()
+{
+  int i;
+
+  char t[] = "\tHello world\n";
+  char r[1000], s[1000];
+
+  escape(s,t);
+  printf("%s\n", s);
+  antiescape(r,s);
+  printf("%s\n", r);
+  
+  return 0;
+}
+
+void escape(char s[], char t[])
+{
+  int i, j;
+  for (i=j=0; t[i]!='\0'; ++i, ++j)
+    switch (t[i]) {
+    case '\n':
+      s[j++] = '\\';
+      s[j] = 'n';
+      break;
+    case '\t':
+      s[j++] = '\\';
+      s[j] = 't';
+      break;
+    default:
+      s[j] = t[i];
+      break;
+    }
+  s[j] = '\0';
+}
+
+void antiescape(char s[], char t[])
+{
+  int i, j;
+  for (i=j=0; t[i]!='\0'; ++i, ++j)
+    switch (t[i]) {
+    case '\\':
+      switch (t[++i]) {
+      case 'n':
+	s[j] = '\n';
+	break;
+      case 't':
+	s[j] = '\t';
+	break;
+      default:
+	s[j++] = '\\';
+	s[j] = t[i];
+	break;
+      }
+      break;
+    default:
+      s[j] = t[i];
+      break;
+    }
+  s[j] = '\0';
+}
