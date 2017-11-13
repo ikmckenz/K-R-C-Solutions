63ae1f1fd11bed4475bbca7189959bc92cb32818
ikmckenz <sirreal.ian@gmail.com>
2017-11-12 22:20:30 -0700

    add 5-7

---
+++ Chapter5/5-07.c
@@ -0,0 +1,108 @@
+/* Rewrite readlines to store lines in an array supplied by main,
+   rather than calling alloc to maintain storage. */
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAXLINES 5000
+
+char *lineptr[MAXLINES];
+
+int readlines(char *lineptr[], int nlines);
+void writelines(char *lineptr[], int nlines);
+void qsort(char *lineptr[], int left, int right);
+
+
+main()
+{
+  int nlines;
+
+  if((nlines = readlines(lineptr, MAXLINES)) >= 0) {
+    qsort(lineptr, 0, nlines-1);
+    writelines(lineptr, nlines);
+    return 0;
+  }
+  else {
+    printf("error: input too big\n");
+    return 1;
+  }
+}
+
+
+/* ------------------------------------------------------------ */
+
+
+#define MAXLEN 1000
+int getline(char *, int);
+
+char linearray[MAXLINES][MAXLEN];
+
+
+int readlines(char *lineptr[], int maxlines)
+{
+  int len, nlines;
+  char *p, line[MAXLEN];
+
+  nlines = 0;
+  while ((len = getline(linearray[nlines], MAXLEN)) > 0) {
+    if (nlines >= maxlines) {
+      return -1;
+    }
+    else {
+      linearray[nlines][len-1] = '\0';
+      lineptr[nlines] = linearray[nlines];
+      nlines++;
+    }
+  }
+  return nlines;
+}
+
+
+void writelines(char *lineptr[], int nlines)
+{
+  while (nlines-- > 0)
+    printf("%s\n", *lineptr++);
+}
+
+
+void qsort(char *v[], int left, int right)
+{
+  int i, last;
+  void swap(char *v[], int left, int right);
+
+  if (left>=right)
+    return;
+  swap(v, left, (left+right)/2);
+  last = left;
+  for (i=left+1; i<=right; i++)
+    if (strcmp(v[i], v[left]) < 0)
+      swap(v, ++last, i);
+  swap(v, left, last);
+  qsort(v, left, last-1);
+  qsort(v, last+1, right);
+}
+
+
+void swap(char *v[], int i, int j)
+{
+  char *temp;
+
+  temp = v[i];
+  v[i] = v[j];
+  v[j] = temp;
+}
+
+
+int getline(char *s, int lim)
+{
+  int c, i;
+
+  for (i=0; i<lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
+    s[i] = c;
+  if (c=='\n') {
+    s[i] = c;
+    ++i;
+  }
+  s[i] = '\0';
+  return i;
+}
